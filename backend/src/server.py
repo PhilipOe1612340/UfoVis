0ce2f7e84ae8c52cd5b060337704c36d16f9bf37
Marius Hahn <marius.hahn@live.com>
2021-01-07 23:45:01 +0100

    add popup do marker

--- backend/src/server.py
+++ backend/src/server.py
@@ -3,19 +3,19 @@ from flask_cors import CORS
 from psycopg2 import sql
 import psycopg2
 import json
-from model.report import Report
+from orm.report import Report
 from sqlalchemy import func
 from db import Session
 
 app = Flask(__name__)
 CORS(app, send_wildcard=True)
 
-@app.route('/someSights')
-def someSights():
+@app.route('/reports')
+def reports():
     session = Session()
-    query = session.query(func.st_asgeojson(Report.point)).limit(1000)
-    coordinates = [json.loads(coordinate[0])["coordinates"] for coordinate in query]
-    return jsonify(coordinates), 200
+    query = Report.query(session).limit(1000)
+    reports = tuple(Report.row_to_dict(report) for report in query)
+    return jsonify(reports), 200
 
 @app.route('/')
 def send_index():
