94bf82eb52b4fcc58a100c0b55bbe0b078f51451
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-01-21 13:05:51 +0100

    replace heatmap lib
    closes #4

--- frontend/src/app/ufo-map/ufo-map.component.ts
+++ frontend/src/app/ufo-map/ufo-map.component.ts
@@ -1,8 +1,62 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import * as L from "leaflet";
 import { DataService, Report } from '../data.service';
-import 'leaflet.heat/dist/leaflet-heat.js'
 import { ConfigService } from '../config/config.service';
+
+declare var HeatmapOverlay: any;
+
+/**
+     * Gradients from https://docs.microsoft.com/en-us/bingmaps/v8-web-control/map-control-concepts/heat-map-module-examples/heat-map-color-gradients
+     */
+
+const aqua = {
+  '0': 'Black',
+  '0.5': 'Aqua',
+  '1': 'White'
+}
+const blue_red = {
+  '0.0': 'blue',
+  '1': 'red'
+}
+const deepSea = {
+  '0.0': 'rgb(0, 0, 0)',
+  '0.6': 'rgb(24, 53, 103)',
+  '0.75': 'rgb(46, 100, 158)',
+  '0.9': 'rgb(23, 173, 203)',
+  '1.0': 'rgb(0, 250, 250)'
+}
+const colorSpectrum = {
+  '0': 'Navy',
+  '0.25': 'Blue',
+  '0.5': 'Green',
+  '0.75': 'Yellow',
+  '1': 'Red'
+}
+const incandescent = {
+  '0': 'Black',
+  '0.4': 'Purple',
+  '0.6': 'Red',
+  '0.8': 'Yellow',
+  '1': 'White'
+}
+const sunrise = {
+  '0': 'Red',
+  '0.66': 'Yellow',
+  '1': 'White'
+}
+const visibleSpectrum = {
+  '0.00': 'rgb(255,0,255)',
+  '0.25': 'rgb(0,0,255)',
+  '0.50': 'rgb(0,255,0)',
+  '0.75': 'rgb(255,255,0)',
+  '1.00': 'rgb(255,0,0)'
+}
+
+export interface Coordinate {
+  x: number;
+  y: number;
+  value: number;
+}
 
 @Component({
   selector: 'ufo-map',
@@ -12,7 +66,11 @@ import { ConfigService } from '../config/config.service';
 export class UfoMapComponent implements OnInit, AfterViewInit {
   public options = {
     layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+        detectRetina: true
+      })
     ],
     zoom: 3,
     center: L.latLng(30, 0)
@@ -41,15 +99,15 @@ export class UfoMapComponent implements OnInit, AfterViewInit {
 
 
   private async repaint(changed = false) {
+    const show = this.config.getSetting('showMarkers');
     const shape = this.config.getSetting("displayShape");
     this.data = await this.service.getData({
       params: {
         fromYear: this.config.getSetting("startYear"),
         toYear: this.config.getSetting("stopYear"),
         shape: shape === "*" ? undefined : shape,
-      }, forceFetch: changed
+        limit: show ? '1000' : '10000',
     })
-    const show = this.config.getSetting('showMarkers')
     if (show) {
       this.showIcons();
     } else {
@@ -58,68 +116,28 @@ export class UfoMapComponent implements OnInit, AfterViewInit {
   }
 
   public showHeatmap() {
-
-    /**
-     * Gradients from https://docs.microsoft.com/en-us/bingmaps/v8-web-control/map-control-concepts/heat-map-module-examples/heat-map-color-gradients
-     */
-
-    const aqua = {
-      '0': 'Black',
-      '0.5': 'Aqua',
-      '1': 'White'
-    }
-    const blue_red = {
-      '0.0': 'blue',
-      '1': 'red'
-    }
-    const deepSea = {
-      '0.0': 'rgb(0, 0, 0)',
-      '0.6': 'rgb(24, 53, 103)',
-      '0.75': 'rgb(46, 100, 158)',
-      '0.9': 'rgb(23, 173, 203)',
-      '1.0': 'rgb(0, 250, 250)'
-    }
-    const colorSpectrum = {
-      '0': 'Navy',
-      '0.25': 'Blue',
-      '0.5': 'Green',
-      '0.75': 'Yellow',
-      '1': 'Red'
-    }
-    const incandescent = {
-      '0': 'Black',
-      '0.4': 'Purple',
-      '0.6': 'Red',
-      '0.8': 'Yellow',
-      '1': 'White'
-    }
-    const sunrise = {
-      '0': 'Red',
-      '0.66': 'Yellow',
-      '1': 'White'
-    }
-    const visibleSpectrum = {
-      '0.00': 'rgb(255,0,255)',
-      '0.25': 'rgb(0,0,255)',
-      '0.50': 'rgb(0,255,0)',
-      '0.75': 'rgb(255,255,0)',
-      '1.00': 'rgb(255,0,0)'
-    }
-
     // maybe make gradient configurable
-    const config = {
-      radius: 30,
-      blur: 30,
-      gradient: incandescent
+    const heatmapConfig = {
+      radius: 26,
+      maxOpacity: 0.8,
+      blur: 0.9,
+      useLocalExtrema: true,
+      latField: 'latitude',
+      lngField: 'longitude',
+      valueField: 'duration',
+      gradient: incandescent,
+      onExtremaChange:
+        (range: { min: number, max: number, gradient: { [key: string]: string } }) => {
+          this.range = range;
+        },
     };
 
     // @ts-ignore
-    this.layers = [L.heatLayer(this.data.map((report) => [report.latitude, report.longitude, report.duration]), config)];
-    this.range = {
-      min: this.data.reduce((min, curr) => curr.duration && curr.duration < min ? curr.duration : min, Infinity),
-      max: this.data.reduce((max, curr) => curr.duration > max ? curr.duration : max, -Infinity) / 3600
-    }
-    this.updateLegend(config.gradient);
+    const heatmapLayer = new HeatmapOverlay(heatmapConfig);
+    this.layers = [heatmapLayer];
+    const aggregate = this.config.getSetting("aggregate");
+    heatmapLayer.setData({ max: aggregate ? this.data.reduce((max, curr) => max > curr.duration ? curr.duration : max, 0) : 1, data: this.data });
+    this.updateLegend(heatmapConfig.gradient);
   }
 
   public showIcons() {
