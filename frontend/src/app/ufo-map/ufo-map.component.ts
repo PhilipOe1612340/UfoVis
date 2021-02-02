fb04e5a75e7ab4b2d823c1287053fc7a66d1ebd7
Marius Hahn <marius.hahn@live.com>
2021-02-02 12:02:26 +0100

    call show heat map

--- frontend/src/app/ufo-map/ufo-map.component.ts
+++ frontend/src/app/ufo-map/ufo-map.component.ts
@@ -222,6 +222,7 @@ export class UfoMapComponent implements OnInit, AfterViewInit {
         limit: show ? '1000' : '10000',
       }, forceFetch: changed, aggregate
     })
+    this.showHeatmap();
   }
 
   public showHeatmap() {
