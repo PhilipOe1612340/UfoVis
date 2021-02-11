0736cd0182d7dfad41b35157efb5ce0f77acaf38
Philip Oesterlin <philip.oesterlin@gmail.com>
2021-02-11 21:29:55 +0100

    better chunking

--- database/preprocessing/scraper/upload.js
+++ database/preprocessing/scraper/upload.js
@@ -12,16 +12,16 @@ async function run() {
     const bar = new Progress.SingleBar({}, Progress.Presets.shades_classic);
     bar.start(queries.length, 0);
     
-    const client_insertion = new PG.Client("postgres://gis_user:gis_pass@localhost:25432/gis_db");
-    await client_insertion.connect();
     
     for (const chunk of chunks) {
+        const client_insertion = new PG.Client("postgres://gis_user:gis_pass@localhost:25432/gis_db");
+        await client_insertion.connect();
         await client_insertion.query(chunk.join('\n'))
             .catch(e => {
                 console.error(e.stack);
                 process.exit(1);
             });
-
+        await client_insertion.end();
         bar.increment(chunk.length);
     }
     bar.stop();
